a13eb9bb375fc8660205cc62368f06e356ebb59d
cehdly99 <mohamed.chedliiy@gmail.com>
2021-03-30 14:26:38 +0100

    sql test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Setup Database (tets)
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* to 'hbnb_test'@'localhost';
